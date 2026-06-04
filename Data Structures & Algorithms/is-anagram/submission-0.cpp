4d313980f13a3deb2a49af85077913e2a2930002
Shiv Arora <155656628+shivabtech23@users.noreply.github.com>
2026-06-04 23:07:45 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     if(s.length()!=t.length()){
+        return false;
+     }  
+
+     vector<int> freq(26,0);
+     for(int i=0;i<s.length();i++){
+        freq[s[i] -'a']++;
+     }
+     for(int j=0;j<t.length();j++){
+        freq[t[j] -'a']--;
+     }
+
+    for(int i=0;i<26;i++){
+        if(freq[i]!=0){
+            return false;
+        }
+    }
+    return true;
+    }
+};
